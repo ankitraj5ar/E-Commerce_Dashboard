67f50de5f4b8962c6e27129453e18ef408ff549c
ankitraj5ar <ankitraj6241@gmail.com>
2023-02-11 15:31:40 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+app.listen(5000,(req,res)=>{
+    console.log("server started at port 5000");
+})
